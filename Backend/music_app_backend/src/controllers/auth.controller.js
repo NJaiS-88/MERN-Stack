3ca75d88361e741f11bd1fe2b3d13c4b1e0e89f8
Jai Sumanth Nekkanti <jaisumanthnekkanti@gmail.com>
2026-08-25 21:54:44 +0530

    log in route added in auth api in music_app_backend

--- Backend/music_app_backend/src/controllers/auth.controller.js
+++ Backend/music_app_backend/src/controllers/auth.controller.js
@@ -1,8 +1,8 @@
 const userModel = require('../models/user.model');
 const bcrypt = require('bcryptjs');
-
+const jwt = require('jsonwebtoken');
 async function registerUser(req, res) {
-    const { username, password, role="user" } = req.body;
+    const { username,email, password, role="user" } = req.body;
     const isUserAlreadyExists = await userModel.findOne({
         $or: [
             {username},
@@ -42,4 +42,37 @@ async function registerUser(req, res) {
     })
 }
 
-module.exports = {registerUser};
+async function loginUser(req, res){
+    const {username, email, password} = req.body;
+    const user = await userModel.findOne({
+        $or: [
+            {username},
+            {email}
+        ]
+    });
+    if(!user){
+        return res.status(404).json({
+            message: "User not found"
+        })
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if(!isPasswordValid){
+        res.status(401).json({
+            message: "Invalid credentials"
+        });
+    }
+    const token = jwt.sign({
+        id: user._id
+    }, process.env.JWT_SECRET);
+    res.cookie("token", token);
+    res.status(200).json({
+        message: "User successfully logged in",
+        user:{
+            id: user._id,
+            username: user.username,
+            email : user.email,
+            role: user.role
+        }
+    });    
+}
+module.exports = {registerUser, loginUser};
